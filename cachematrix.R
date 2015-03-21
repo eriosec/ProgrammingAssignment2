8408dcc94581c33a0b6a4d94774bcb5ebf6a967c
eriosec <eriosec@verizon.net>
2015-03-21 13:44:01 -0400

    Fuller comments explaining functions made and variable names changed

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 
+## Overall functions Descriptions:
 
-## makeCacheMatrix is a function that gets passed an argument of type matrix
-## named x, and does the following:
+## A.  "makeCacheMatrix" is a function that takes an argument of type matrix
+##      named x, and does the following:
 ## 1. It INITIALIZES a matrix of length 0 and assigns it to a matrix variable
 ##    named invmatrix into which the inverse of x will be stored
 ## 2. It then defines a function named "set" into which the matrix argument of
@@ -15,14 +15,38 @@
 ## 3.  It then defines a function named "get" which returns the matrix that was
 ##     cached in the "set" function
 ## 4.  It then defines a function named "setinverse" which gets passed an inverse
-##     matrix and binds it to the invmatrix name that resides in the parent 
-##     environemtn of "set" which is also the execution environment of the 
+##     matrix and binds it to the invmatrix name that resides in its parent 
+##     environemtn which is also the execution environment of the 
 ##     function "makeCacheMatrix. This in effect caches the inverse matrix
 ## 5.  It then defines a function named "getinverse" which returns the inverse
 ##     matrix that was cached with the "setinverse" function
 ## 6.  At the end, the function makeCacheMatrix returns a list of the "set", "get",
 ##     "setinverse", and "getinverse" functions.
 
+## B. "cacheSolve" is a function that takes an argument x of type list which
+##    needs to have been created using the "makeCacheMatrix" function and then
+##    does the following:
+## 1. It checks the value of the inverse matrix that was returned through
+##    the function "getinverse" in the list of values of the argument x that
+##     was passed to "cacheSolve"
+## 2. If the length of the inverse matrix is not 0, i.e a 0x0 matrix,
+##     then the function ends by returning the inverse matrix that is cached
+##     in the "getinverse" function 
+## 3. If the length of inverse matrix is zero then the value of the matrix
+##    that was originally passed to the "makeCacheVector" function is accessed 
+##    and assigned the name: xmatrix. 
+## 4. xmatrix is passed to the function solve() to calculate the inverse of
+##    xmatrix and the returned matrix is assigned the name: inverse
+## 5. The inverse is then cached using the "setinverse" function which is 
+##    in the list of values of the argument x that was passed to "cacheSolve"
+## 6. Finally, the inverse is returned
+##
+
+## Short Descrition of "makeCacheMatrix":
+
+## When bound to a name, "makeCacheMatrix" caches the matrix that gets passed as 
+## argument and intitializes its inverse to  a 0x0 matrix and makes both
+## available through the lists of functions that are returned.
 
 makeCacheMatrix <- function(x = matrix()) {
     
@@ -57,23 +81,31 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Short description of "cacheSolve":
+
+## "cacheSolve" is a function that takes an argument x of type list which
+##  needs to have been created using the "makeCacheMatrix" function so that  
+##  the matrix originally passed to "makeCacheMatrix" can be accessed and
+##  its inverse returned either from cache or through calculation which is 
+##  then cached through the "setinverse" function that was returned in 
+##  the list of values of x.
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     
-    invmatrix <- x$getinverse()
+    inverse <- x$getinverse()
     
-    if(length(invmatrix)!=0) {
+    if(length(inverse)!=0) {
         message("getting cached matrix")
-        return(invmatrix)
+        return(inverse)
     }
     
-    data <- x$get()
+    xmatrix <- x$get()
     
-    invmatrix <- solve(data)
+    inverse <- solve(xmatrix)
     
-    x$setinverse(invmatrix)
+    x$setinverse(inverse)
     
-    invmatrix
+    inverse
 }
